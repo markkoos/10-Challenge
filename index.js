f3c54eb081a55c94f71481e3e14284da7c6656aa
markkoos <markusimercado@gmail.com>
2022-11-27 14:22:40 -0800

    finished manager function; working on others

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const inquirer = require("inquirer");
 const fs = require(`fs`);
 const Employee = require(`./lib/Employee`);
+const Manager = require(`./lib/Manager`);
+const Intern = require(`./lib/Intern`);
+const Engineer = require(`./lib/Engineer`);
 const employeeQuestions = require(`./src/employeeQuestions`)
 const engineerQuestions = require(`./src/engineerQuestions`)
 const internQuestions = require(`./src/internQuestions`)
@@ -12,12 +15,41 @@ function writeToFile(team) {
 }
 
 function executeInquirers() {
-    var teamMembers = []
-    const addManager = inquirer.prompt(managerQuestions) 
+    // array to push the team members into
+    var team = [];
+
+    function addManager() {
+        inquirer.prompt(managerQuestions) 
         .then((data) => {
-            teamMembers += JSON.stringify(data);
-            console.log(teamMembers);
+            const newManager = 
+            new Manager (
+                data.managerName,
+                data.managerId,
+                data.managerEmail,
+                data.OfficeNumber
+            )
+            team.push(newManager);
+            console.log(team);
+            addMember();
         })
+    }
+
+    function addMember() {
+        inquirer.prompt(employeeQuestions)
+        .then((data) => {
+            if (data.choice === "Engineer") {
+
+            }
+        })
+    }
+
+    function addEngineer() {
+        
+    }
+
+    function addIntern() {
+
+    }
 }
 
 executeInquirers();
